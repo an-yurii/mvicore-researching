f9cba5385fb71efc2c3344373a33b13d3b0f7139
Yurii Anisimov <31529762+an-yurii@users.noreply.github.com>
2020-05-04 19:41:25 +0700

    menu selection handling

--- startscreen/src/main/java/com/yurii/mvicoreresearching/startscreen/ui/StartFragment.kt
+++ startscreen/src/main/java/com/yurii/mvicoreresearching/startscreen/ui/StartFragment.kt
@@ -5,10 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.yurii.mvicoreresearching.characters_api.CharactersFeatureApi
+import com.yurii.mvicoreresearching.episodes_api.EpisodesFeatureApi
+import com.yurii.mvicoreresearching.locations_api.LocationsFeatureApi
 import com.yurii.mvicoreresearching.startscreen.R
 import com.yurii.mvicoreresearching.startscreen.di.StartScreenFeatureComponent
+import kotlinx.android.synthetic.main.fragment_start.*
+import javax.inject.Inject
 
 class StartFragment : Fragment() {
+
+    @Inject
+    lateinit var charactersFeatureApi: CharactersFeatureApi
+    @Inject
+    lateinit var locationsFeatureApi: LocationsFeatureApi
+    @Inject
+    lateinit var episodesFeatureApi: EpisodesFeatureApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         StartScreenFeatureComponent.Initializer.get().inject(this)
@@ -18,5 +30,20 @@ class StartFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_start, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
+            R.id.characters_item
+            val fragment = when (menuItem.itemId) {
+                R.id.characters_item -> charactersFeatureApi.charactersStarter().getFragment()
+                R.id.locations_item -> locationsFeatureApi.locationsStarter().getFragment()
+                R.id.episodes_item -> episodesFeatureApi.episodesStarter().getFragment()
+                else -> null
+            }
+            fragment != null
+        }
+    }
+
 
 }
